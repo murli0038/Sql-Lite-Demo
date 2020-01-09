efb406ee88ac0ec48122cb10b2cdb7b07442eaf7
Nikunj Prajapati <prajapatimurli2000@gmail.com>
2020-01-09 12:11:41 +0530

    first commit

---
+++ SQLiteDemo_New/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  SQLiteDemo_New
+//
+//  Created by Nikunj Prajapati on 09/12/19.
+//  Copyright © 2019 Nikunj Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SQLiteDemo_New/ManageDB.swift
@@ -0,0 +1,13 @@
+//
+//  ManageDB.swift
+//  SQLiteDemo_New
+//
+//  Created by Nikunj Prajapati on 16/10/19.
+//  Copyright © 2019 Nikunj Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class ManageDB: NSObject {
+
+}
